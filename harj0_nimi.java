d4b0b24dc78ca31372acc54bd6aafbc1634480d0
unhart <antony.hussi@gmail.com>
2019-01-07 17:15:38 +0200

    Some changes

---
+++ harj0_nimi.java
@@ -0,0 +1,14 @@
+// _RUE_
+// Anton Hussi
+// 2019.01.07
+public class harj0_nimi {
+  public static void main(String[] args) {
+    // Variables
+    String Name = "Anton Hussi";
+    String Age = "Almost 40's";
+    String PostAddr = "Arkkitehdinkatu 16 A 7";
+    String PostNro = "33720 Tampere";
+    // Printing
+    System.out.printf(Name + "\r" + Age + "\r" + PostAddr + "\r" + PostNro + "\r");
+  }
+}
