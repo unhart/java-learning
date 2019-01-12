56667e05420d683fb657f98218829b4a4f3c3e8d
unhart <antony.hussi@gmail.com>
2019-01-12 19:21:26 +0200

    More changes

--- harj2_4.java
+++ harj2_4.java
@@ -2,26 +2,20 @@
 // Anton Hussi :: 17TVDO02 :: 15DAPT232.01
 // Created at 2019.01.11 :: Update at 2019.01.11
 // harjoitus 2_4 : Luvun toisto
+
 public class harj2_4 {
   public static void main(String[] args) {
     // punch of variables and user inputs
-<<<<<<< HEAD
-=======
-    int nro_count = 10;
->>>>>>> 3ef2d10e1ce271a8833cf9fa6e4c6fb08e8cc894
     System.out.print("Syötä luku väliltä 1-5: ");
     int nro = Reader.readInt();
     // downgrade harj2_1-filee
     if(nro > 1 && nro < 5) {
-<<<<<<< HEAD
       int i = 1;
       while(i < 101) {
         System.out.println("Arvo on nyt " + i);
         i++;
       }
 
-=======
->>>>>>> 3ef2d10e1ce271a8833cf9fa6e4c6fb08e8cc894
       while(i < 5) {
         System.out.println("Arvo on " + i);
         i++;
@@ -30,14 +24,11 @@ public class harj2_4 {
       System.out.println("Arvo ei ole väliltä 1-5");
       return;
     }
-<<<<<<< HEAD
 
     while(i < 100) {
       System.out.print("Syötä arvo: ");
       int nro = Reader.readInt();
       if(nro >)
     }
-=======
->>>>>>> 3ef2d10e1ce271a8833cf9fa6e4c6fb08e8cc894
   }
 }
