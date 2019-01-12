faa8a4fab5c63171842153dd977fe3541d267f15
unhart <antony.hussi@gmail.com>
2019-01-12 23:07:54 +0200

    New files and addons!

--- harj1_2.java
+++ harj1_2.java
@@ -1,47 +1,69 @@
 // Java-learning
 // Anton Hussi :: 17TVDO02 :: 15DAPT232.01
 // Created at 2019.01.11 :: Update at 2019.01.12
+// Harjoitus 1_2 | Ympyrän pinta-ala
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Scanner;
 // actial class
 public class harj1_2 {
+  // Variables for class
+  static String[] _errorMSG = {"Virhe:", "Syötit tekstiä, anna lukuina", "Syötit väärän SI-yksikön", "Syötit väärän mittayksikön"};
+  static String[] _SIvalues = {"mm", "millimetri", "cm", "senttimetri", "m", "metri", "km", "kilometri"};
   // Calling of imported DecimalFormat-class and "variable" for it
-  private static DecimalFormat _round = new DecimalFormat(".##");
+  private static DecimalFormat _round = new DecimalFormat("#.##");
   // Actual main program
   public static void main(String[] args) {
+    // Calling of imported DecimalFormat-class and "variable" for it
     // Printing first line and statement purpose of program
     System.out.println(":: Lasketaan ympyrän ala");
     // user input for radius
     // User input for unit of measurement
-    System.out.print("Syötä haluamasi SI-mittayksikkö, esimerkiksi mm, metri tai km: ");
+    System.out.println("Haluamasi SI-mukainen pituusysikkö, joko lyhennettynät tai kokonaisuutena.");
+    System.out.print("Syötä yksikkö: " + "\t\t");
     String _SI = Reader.readString();
-    String[] _SIvalues = {"mm", "millimetri", "senttimetri", "cm", "m", "metri", "km", "kilometri"};
+    Scanner _SIformat = new Scanner(_SI);
     // Checking of user inputs..
-    switch(_SI) {
-      case isEmpty():
-        System.out.println("Syötä mittayksikkö");
-        break;
-      case ArrayUtils.contains(_SIvalues, _SI):
-        // user input for radius
-        System.out.print("Syötä säde: ");
-        double _radius = Reader.readDouble();
-        double _area = Math.PI * (_radius * _radius);
-        // Printing out user inpuied radius and then sum of area from it
-        System.out.println("Ympyrän säde on: " + "\t" + _round.format(_radius) + " " + _SI);
-        System.out.println("Ympyrän pinta-ala on: " + "\t" + _round.format(_area) + " " + _SI + "2");
-        break;
+    if(! _SIformat.hasNextDouble() || ! _SIformat.hasNextBigInteger()) {
+      if(_SI != null && ! _SI.isEmpty()) {
+    //      boolean _SIcheck = Arrays.stream(_SIvalues).anyMatch(_SI::equals);
+          // Checking of that SI-unit is allowed on the array
+          // TRUE = calculating
+          // FALSE = Printing out error-msh
+          if(Arrays.stream(_SIvalues).anyMatch(_SI::equals)) {
+            // user input for radius
+            System.out.print("Syötä säde: " + "\t\t");
+            double _radius = Reader.readDouble();
+            double _area = Math.PI * (_radius * _radius);
+            System.out.println(":: Ympyrän mitat on");
+            // Printing out user inpuied radius and then sum of area from it
+            System.out.println("Ympyrän säde on: " + "\t" + _round.format(_radius) + " " + _SI);
+            System.out.println("Ympyrän pinta-ala on: " + "\t" + _round.format(_area) + " " + _SI + "2");
+          } else {
+            System.out.println(_errorMSG[0] + _errorMSG[2]);
+          }
+        } else {
+          System.out.println(_errorMSG[0] + _errorMSG[3]);
+      }
+
+    } else {
+      System.out.println(_errorMSG[0] + _errorMSG[1]);
     }
-    // if(_SI != null && !_SI.isEmpty()) {
-    //   if(S)
-    //   // user input for radius
-    //   System.out.print("Syötä säde: ");
-    //   double _radius = Reader.readDouble();
-    //   double _area = Math.PI * (_radius * _radius);
-    //   // Printing out user inpuied radius and then sum of area from it
-    //   System.out.println("Ympyrän säde on: " + "\t" + _round.format(_radius) + " " + _SI);
-    //   System.out.println("Ympyrän pinta-ala on: " + "\t" + _round.format(_area) + " " + _SI + "2");
-    // } else {
-    //   System.out.println("Metsään meni");
+    _SIformat.close();
+    // case didn't work as planned, need to come back and check it at later on
+    // switch(_SI) {
+    //   case isEmpty():
+    //     System.out.println("Syötä mittayksikkö");
+    //     break;
+    //   case ArrayUtils.contains(_SIvalues, _SI):
+    //     // user input for radius
+    //     System.out.print("Syötä säde: ");
+    //     double _radius = Reader.readDouble();
+    //     double _area = Math.PI * (_radius * _radius);
+    //     // Printing out user inpuied radius and then sum of area from it
+    //     System.out.println("Ympyrän säde on: " + "\t" + _round.format(_radius) + " " + _SI);
+    //     System.out.println("Ympyrän pinta-ala on: " + "\t" + _round.format(_area) + " " + _SI + "2");
+    //     break;
     // }
   }
 }
--- harj1_3.java
+++ harj1_3.java
@@ -1,14 +1,46 @@
 // Java-learning
 // Anton Hussi :: 17TVDO02 :: 15DAPT232.01
-// Created at 2019.01.11 :: Update at 2019.01.11
-import Reader.java
-
+// Created at 2019.01.11 :: Update at 2019.01.12
+// Harjoitus 1-3 | Bensaa suonissa
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Scanner;
+// actial class
 public class harj1_3 {
-  public static long round(dobule a)
+  // Variables for class
+  static String[] _errorMSG = {"Virhe:", "Annappas decimaaleina", "", "Tarkista syötteesi"};
+  private static DecimalFormat _round = new DecimalFormat("#.##");
+  // Actual main program
+  public static void main(String[] args) {
+    // Printing first line and statement purpose of program
+    //DecimalFormat _round = new DecimalFormat(".##");
+//    DecimalFormatSymbols _symbol = new DecimalFormatSymbols(fi);
 
-  public void main() {
-
-
-
+    // Calling of imported DecimalFormat-class and "variable" for it
+    System.out.println(":: Naftalaskuri, 0.1B");
+    System.out.println("Pekan rahat oli tiukissa, kun Elina tahtoi rahaa Moskovan torille ja naftaa pitäisi ostaa Ladaan.");
+    // Asking user input for a couple parameters
+    System.out.print("Syötä päivän bensanhinta (anna desimaaleina): " + "\t\t\t");
+    double _GASprice = Reader.readDouble();
+    double _GASprefor = Double.valueOf(_GASprice);
+    Integer _GASformaz = Integer.valueOf(_GASprefor.intvalue());
+//    Scanner _GASformat = new Scanner(_GASformaz);
+    // Checking of user inputs..
+    if(_GASformaz == (int)_GASformaz) {
+//    if(_GASprice == Double.valueOf(_GASprice)) {
+        // user input for radius
+        System.out.print("Päivän lompakon sisältö (kokonaislukuna tai desimaaleina): " + "\t");
+        double _GASwallet = Reader.readDouble();
+        System.out.println("\r");
+        if(_GASwallet == Double.valueOf(_GASwallet)) {
+          // Printing out answer based on amount of money and divede by price of Gas
+          double _GASfinal = _GASwallet / _GASprice;
+          System.out.println("Pekka saa " + _round.format(_GASwallet) + " Eurolla, about " + _round.format(_GASfinal) + " litraa naftaa Ladaansa");
+        } else {
+          System.out.println(_errorMSG[0] + _errorMSG[3]);
+        }
+    } else {
+       System.out.println(_errorMSG[0] + _errorMSG[1]);
+    }
   }
 }
